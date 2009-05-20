630b36a74748d4d743ee479dfb92dbb20e8b4d52
Kyle Fuller <inbox@kylefuller.co.uk>
2009-05-20 13:16:17 +0100

    Few bug fixes

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
         'template_object_name': 'project',
     }, 'project_list'),
     url(r'^update/(?P<slug>[-\w]+)/$', 'projects.views.update'),
-    url(r'^(?P<project>[-\w]+)/$', 'projects.views.document', {'url': ''}, 'project_detail'),
+    url(r'^(?P<project>[-\w]+)/$', 'projects.views.document', {'url': 'index'}, 'project_detail'),
     url(r'^(?P<project>[-\w]+)/(?P<url>[\w./-]*)/$', 'projects.views.document', name='doc_detail'),
 )
